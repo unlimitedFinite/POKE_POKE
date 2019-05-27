148d7598e59c594247fe1f2754640597c7957466
Daniele Deltodesco <deaddread666@gmail.com>
2019-05-27 17:59:48 +0800

    added landing page image

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pokemon#index'
+  root to: 'pages#home'
   resources :bookings, except: %i[edit update]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
