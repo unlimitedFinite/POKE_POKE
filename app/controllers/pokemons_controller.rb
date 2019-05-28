208f81d60b9395b91e6cbffd24edf1586aade802
Leo Chan <lchan168@hotmail.com>
2019-05-28 12:17:12 +0800

    update policy on pokemon controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,5 @@
 class PokemonsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-  before_action :set_pokemon, only: [:show, :edit, :update, :destroy]
 
   def index
     @pokemons = policy_scope(Pokemon)
