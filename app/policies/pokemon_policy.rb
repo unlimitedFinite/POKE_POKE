b46baa6038b5b76397210c340688389085995e4d
Leo Chan <lchan168@hotmail.com>
2019-05-28 14:18:29 +0800

    new pokemon policy

--- app/policies/pokemon_policy.rb
+++ app/policies/pokemon_policy.rb
@@ -5,31 +5,31 @@ class PokemonPolicy < ApplicationPolicy
     end
   end
 
-    def index?
-      true
-    end
+  def index?
+    true
+  end
 
-    def show?
-      true
-    end
+  def show?
+    true
+  end
 
-    def new?
-      record.user == user
-    end
+  def new?
+    record.user == user
+  end
 
-    def create?
-      record.user == user
-    end
+  def create?
+    record.user == user
+  end
 
-    def edit?
-      record.user == user
-    end
+  def edit?
+    record.user == user
+  end
 
-    def update?
-      record.user == user
-    end
+  def update?
+    record.user == user
+  end
 
-    def destroy?
-      record.user == user
-    end
+  def destroy?
+    record.user == user
+  end
 end
