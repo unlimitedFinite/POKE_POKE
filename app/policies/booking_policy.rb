461e0758f857715b592c2c4bc74b731c3cea92b5
Daniele Deltodesco <deaddread666@gmail.com>
2019-05-29 13:17:44 +0800

    changed scope of booking policy so that index only shows logged in users bookings

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,8 +1,7 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
-      # handles booking index view after booking is made
+      scope.where(user: user)
     end
   end
 
