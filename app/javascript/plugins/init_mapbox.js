a460e1946ea440ecd268bfef58f8e197b46f9d1f
Leo Chan <lchan168@hotmail.com>
2019-05-29 13:21:53 +0800

    map initialization

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,38 @@
+import mapboxgl from 'mapbox-gl';
+
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+    });
+
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+  }
+    const addMarkersToMap = (map, markers) => {
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup) // add this
+          .addTo(map);
+      });
+    };
+
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
+}
+
+export { initMapbox };
+
