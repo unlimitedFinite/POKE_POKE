aac02449c29053b7a90bc2252b3f0fca8ba73b67
Jake Spector <spector.jake@gmail.com>
2019-05-31 16:02:28 +0800

    commenting out algolia config

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: ENV['algolia_application_id'], api_key: ENV['algolia_api_key'] }
+# AlgoliaSearch.configuration = { application_id: ENV['algolia_application_id'], api_key: ENV['algolia_api_key'] }
