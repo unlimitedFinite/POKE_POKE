f1663012003228f40f4a85098eae5b7753bbaf13
Jake Spector <spector.jake@gmail.com>
2019-05-31 14:05:04 +0800

    Updating seed file to add reviews and bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,16 @@
 
 require 'open-uri'
 require 'json'
+require 'faker'
+
+puts 'Destroying db contents'
 # Delete records
+Review.destroy_all
 Booking.destroy_all
 Pokemon.destroy_all
 User.destroy_all
 
+puts 'Creating Ash'
 # Create Users
 ash = User.create(
   email: 'ash.ketchum@pokemon.com',
@@ -25,6 +30,7 @@ ash = User.create(
   is_owner: true
 )
 
+puts 'Creating Misty'
 misty = User.create(
   email: 'misty.star@pokemon.com',
   first_name: 'Misty',
@@ -35,7 +41,32 @@ misty = User.create(
   password: 'password'
 )
 
+puts 'Creating other Users'
+5.times do
+  User.create(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.male_first_name,
+    last_name: Faker::Name.last_name,
+    dob: '1/1/1990',
+    phone_number: Faker::PhoneNumber.cell_phone,
+    gender: 'M',
+    password: 'password'
+  )
+end
 
+5.times do
+  User.create(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.female_first_name,
+    last_name: Faker::Name.last_name,
+    dob: '1/1/1990',
+    phone_number: Faker::PhoneNumber.cell_phone,
+    gender: 'F',
+    password: 'password'
+  )
+end
+
+puts 'Creating Pokemon'
 names = []
 url = 'https://pokeapi.co/api/v2/pokemon?limit=50'
 json = open(url).read
@@ -46,12 +77,13 @@ end
 
 # Create Pokemons
 count = 0
+pokemon_list = []
 names.each do |name|
   url = "https://pokeapi.co/api/v2/pokemon/#{name}"
   json = open(url).read
   data = JSON.parse(json)
   pokemon = Pokemon.new(
-    user: ash,
+    user: User.order('RANDOM()').first,
     level: rand(0..25),
     address: Faker::Address.city,
     price_per_day: rand(0..50),
@@ -61,6 +93,52 @@ names.each do |name|
   )
   count += 1
   pokemon.save
+  pokemon_list << pokemon
 end
 
+# Create Bookings
+bookings_list = []
+puts 'Creating active bookings'
+# active bookings
+pokemon_list.each do |pokemon|
+  Booking.create(
+    pokemon: pokemon,
+    user: User.order('RANDOM()').first,
+    start_dt: Faker::Date.between(10.days.ago, Date.today),
+    end_dt: Faker::Date.between(Date.today, 10.days.from_now),
+    price_paid: (Faker::Date.between(Date.today, 10.days.from_now) - Faker::Date.between(10.days.ago, Date.today)) * pokemon.price_per_day
+  )
+end
 
+puts 'Creating past bookings'
+# past bookings
+pokemon_list.each do |pokemon|
+  bookings_list << Booking.create(
+    pokemon: pokemon,
+    user: User.order('RANDOM()').first,
+    start_dt: Faker::Date.between(60.days.ago, Date.new(2019, 4, 30)),
+    end_dt: Date.new(2019, 4, 30),
+    price_paid: (Date.new(2019, 4, 30) - Faker::Date.between(60.days.ago, Date.new(2019, 4, 30))) * pokemon.price_per_day
+  )
+end
+
+puts 'Creating future bookings'
+# future bookings
+pokemon_list.each do |pokemon|
+  Booking.create(
+    pokemon: pokemon,
+    user: User.order('RANDOM()').first,
+    start_dt: Date.new(2019, 7, 30),
+    end_dt: Faker::Date.between(Date.new(2019, 7, 30), 90.days.from_now),
+    price_paid: (Faker::Date.between(Date.new(2019, 7, 30), 90.days.from_now) - Date.new(2019, 7, 30)) * pokemon.price_per_day
+  )
+end
+
+# Creating reviews
+bookings_list.each do |booking|
+  Review.create(
+    booking: booking,
+    content: "#{booking.pokemon.name} was a #{Faker::Dessert.variety}",
+    rating: rand(0..5)
+  )
+end
