d9cd59c57718c412dcf0ce4cfc12f538935f646b
Leo Chan <lchan168@hotmail.com>
2019-06-01 16:03:12 +0800

    date validator

---
+++ app/validators/availability_validator.rb
@@ -0,0 +1,13 @@
+class AvailabilityValidator < ActiveModel::EachValidator
+
+  def validate_each(record, attribute, value)
+    bookings = Booking.where(["pokemon_id =?", record.pokemon_id])
+    date_ranges = bookings.map { |b| b.start_dt..b.end_dt }
+
+    date_ranges.each do |range|
+      if range.include? value
+        record.errors.add(attribute, "not available")
+      end
+    end
+  end
+end
